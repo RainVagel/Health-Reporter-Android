dd5e635ab816de4b23fb6272a444ffbb5e557636
Rain Vagel <rain.vagel@gmail.com>
2016-09-25 18:07:47 +0300

    Created DBContract class and necessarry infrastructure to create a database.

--- app/src/main/java/rainvagel/healthreporter/DBHelper.java
+++ app/src/main/java/rainvagel/healthreporter/DBHelper.java
@@ -9,108 +9,23 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBHelper extends SQLiteOpenHelper {
 
-    static final String dbName = "HealthReporterDB";
-
-    static final String testsTable = "tests";
-    static final String colId = "id";
-    static final String colCategoryID = "categoryId";
-    static final String colName = "name";
-    static final String colDescription = "description";
-    static final String colUnits = "units";
-    static final String colDecimals = "decimals";
-    static final String colWeight = "weight";
-    static final String colFormulaF = "formulaF";
-    static final String colFormulaM = "formulaM";
-    static final String colPosition = "position";
-    static final String colUpdated = "updated";
-    static final String colUploaded = "uploaded";
-
-    static final String ratingsTable = "ratings";
-    static final String colTestId = "testId";
-    static final String colLabelId = "labelId";
-    static final String colAge = "age";
-    static final String colNormF = "normF";
-    static final String colNormM = "normM";
-//    + colUpdated and colUploaded
-
-    static final String ratingLabelsTable = "rating_labels";
-//    + colName
-    static final String colInterpretation = "interpretation";
-//    colUpdated and colUploaded
-
-    static final String presetTestsTable = "preset_tests";
-//    + colTestId
-    static final String colPresetId = "presetId";
-//    + colUpdated and colUploaded
-
-    static final String presetsTable = "presets";
-//    +colId and colName and colUpdated and colUpgraded
-
-    static final String testCategoriesTable = "test_categories";
-//    + colId
-    static final String colParenId = "parentId";
-//    + colName and colPosition and colUpdated and colUploaded
-
-    static final String apprisalTestsTable = "appraisal_tests";
-    static final String colAppraisalId = "appraisalId";
-//    + colTestId
-    static final String colScore = "score";
-    static final String colNote = "note";
-    static final String colTrial1 = "trial1";
-    static final String colTrial2 = "trial2";
-    static final String colTrial3 = "trial3";
-//    + colUpdated + colUploaded
-
-    static final String appraisersTable = "appraisers";
-//    +colId + colName + colUpdated + colUploaded
-
-    static final String appraisalstTable = "appraisals";
-    static final String colAppraiserId = "appraiserId";
-    static final String colClientId = "clientId";
-    static final String colDate = "date";
-//    + colUpdated + colUploaded
-
-    static final String clientsTable = "clients";
-//    + colId
-    static final String colFirstName = "firstName";
-    static final String colLastName = "lastName";
-    static final String colGroupId = "groupId";
-    static final String colEmail = "email";
-    static final String colGender = "gender";
-    static final String colBirthDate = "birthDate";
-//    + colUpdated + colUploaded
-
-    static final String groupsTable = "groups";
-//    +colName + colUpdated + colUploaded
-
     public DBHelper(Context context) {
-        super(context, dbName, null, 0);
+        super(context, DBContract.DATABASE_NAME, null, DBContract.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        Appraisers table
-        db.execSQL("CREATE TABLE " + appraisersTable + "(" + colId + " BLOB PRIMARY KEY NOT NULL, " +
-        colName + " TEXT, " + colUpdated + " DATETIME NOT NULL, " + colUploaded + " DATETIME NOT NULL" + ")");
-
-//        Groups table
-        db.execSQL("CREATE TABLE " + groupsTable + "(" + colId + " BLOB PRIMARY KEY NOT NULL, " +
-        colName + " TEXT, " + colUpdated + " DATETIME NOT NULL, " + colUploaded + " DATETIME NOT NULL" + ")");
-
-//        Rating_labels table
-        db.execSQL("CREATE TABLE " + ratingLabelsTable + "(" + colId + " BLOB PRIMARY KEY NOT NULL, " +
-        colName + " TEXT, " + colInterpretation + " TEXT, " + colUpdated + " DATETIME NOT NULL, " + colUploaded +
-        " DATETIME NOT NULL" + ")");
-
-//        Presets table
-        db.execSQL("CREATE TABLE " + presetsTable + "(" + colId + " BLOCK PRIMARY KEY NOT NULL, " +
-        colName + " TEXT, " + colUpdated + " DATETIME NOT NULL, " + colUploaded + " DATETIME NOT NULL" + ")");
-
-//        Clients table
-        db.execSQL("CREATE TABLE " + clientsTable + "(" + colId + " BLOC PRIMARY KEY NOT NULL, " +
-        colFirstName + " TEXT, " + colLastName + " TEXT, " + colGroupId + " BLOB NOT NULL, FOREIGN KEY (" + colGroupId + ") " +
-                "REFERENCES " + groupsTable + " (" + colId +"), " + colEmail + " TEXT, " + colGender + " TINYINT, " + colBirthDate +
-        " DATE, " + colUpdated + " DATETIME NOT NULL, " + colUploaded + " DATETIME NOT NULL" + ")");
+        db.execSQL(DBContract.Groups.CREATE_TABLE);
+        db.execSQL(DBContract.Clients.CREATE_TABEL);
+        db.execSQL(DBContract.Appraisers.CREATE_TABLE);
+        db.execSQL(DBContract.Appraisals.CREATE_TABLE);
+        db.execSQL(DBContract.TestCategories.CREATE_TABLE);
+        db.execSQL(DBContract.Tests.CREATE_TABEL);
+        db.execSQL(DBContract.RatingLabels.CREATE_TABLE);
+        db.execSQL(DBContract.Ratings.CREATE_TABLE);
+        db.execSQL(DBContract.Presets.CREATE_TABLE);
+        db.execSQL(DBContract.PresetTests.CREATE_TABLE);
+        db.execSQL(DBContract.AppraisalTests.CREATE_TABLE);
     }
 
     @Override
