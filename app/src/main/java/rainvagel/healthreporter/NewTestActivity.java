00f1e26e9e8bbbfc36291e899aeeb54ee80ce5c7
CORRU <corneliaefros@gmail.com>
2016-09-28 18:51:40 +0300

    TestActivity stuff close #42

---
+++ app/src/main/java/rainvagel/healthreporter/NewTestActivity.java
@@ -0,0 +1,37 @@
+package rainvagel.healthreporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewTestActivity extends AppCompatActivity {
+
+    Button create;
+    EditText name;
+    EditText description;
+    EditText units;
+    EditText weight;
+    EditText formulaF; //A test can be a user-supplied formula (a simple equation) which contains the results
+    // of other tests. When a test result is entered, any formulas which contain that test should be evaluated.
+    EditText formulaM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_test);
+    }
+
+    public void create() {
+        create = (Button)findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+
+    }
+}
