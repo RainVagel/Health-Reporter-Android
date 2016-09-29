40e5e421fab5b7d84bb2c8450df57b305fb60014
unknown <kaarel.sormus@gmail.com>
2016-09-29 13:14:21 +0300

    Plus button added to Client Activity. Closes #28

--- app/src/main/java/rainvagel/healthreporter/ClientActivity.java
+++ app/src/main/java/rainvagel/healthreporter/ClientActivity.java
@@ -2,8 +2,6 @@ package rainvagel.healthreporter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
@@ -16,6 +14,7 @@ public class ClientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
 
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
@@ -49,27 +48,4 @@ public class ClientActivity extends AppCompatActivity {
 
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
 }
