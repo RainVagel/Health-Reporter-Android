6ce925b078978acb8b14ede04bfe49cc3af0f24f
unknown <kaarel.sormus@gmail.com>
2016-10-27 13:47:33 +0300

    Fixed a bug - now shows correct people when pressed on a group. Secondly, pressing on a client in groups shows category view of that client.

--- app/src/main/java/rainvagel/healthreporter/GroupClientActivity.java
+++ app/src/main/java/rainvagel/healthreporter/GroupClientActivity.java
@@ -5,6 +5,9 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +17,7 @@ public class GroupClientActivity extends AppCompatActivity {
 
     final ArrayList<Category> categories = new ArrayList<>();
     final ArrayList<String> clientnames = new ArrayList<>();
+    final  ArrayList<Integer> clientIDs = new ArrayList<>();
     Intent toCategories;
     static String[] intentData;
     Toolbar tb;
@@ -34,16 +38,21 @@ public class GroupClientActivity extends AppCompatActivity {
 
 
         DBHelper mydb = new DBHelper(GroupClientActivity.this);
-        String [] clientColumns = {DBContract.Clients.KEY_FIRSTNAME, DBContract.Clients.KEY_LASTNAME,DBContract.Clients.KEY_GROUP_ID};
+        String [] clientColumns = {DBContract.Clients.KEY_FIRSTNAME, DBContract.Clients.KEY_LASTNAME,DBContract.Clients.KEY_GROUP_ID,DBContract.Clients.KEY_ID};
         Cursor res = mydb.getReadableDatabase().query(DBContract.Clients.TABLE_NAME,clientColumns,null,null,null,null,null);
         int clientGroupID = res.getColumnIndex(DBContract.Clients.KEY_GROUP_ID);
+        int rowIndex = res.getColumnIndex(DBContract.Clients.KEY_ID);
         int name = res.getColumnIndex(DBContract.Clients.KEY_FIRSTNAME);
         int famname = res.getColumnIndex(DBContract.Clients.KEY_LASTNAME);
 
 
         for(res.moveToFirst();!res.isAfterLast();res.moveToNext()){
+
             if(res.getString(clientGroupID).equals(intentData[0])){
+                Log.v("GROUPCLIENT",res.getString(name)+" "+res.getString(famname)+
+                        " "+res.getString(clientGroupID));
                 clientnames.add(res.getString(name)+ " "+ res.getString(famname));
+                clientIDs.add(Integer.valueOf(res.getString(rowIndex)));
             }
         }
 
@@ -56,6 +65,21 @@ public class GroupClientActivity extends AppCompatActivity {
 
 
         lv.setAdapter(arrayAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int clientId= clientIDs.get(position);
+                Intent toCategories = new Intent(GroupClientActivity.this, CategoriesActivity.class);
+                // we will pass on client's name,group and id in a string, all separated with a comma.
+                String passedData =(String.valueOf(clientId)+","+clientnames.get(position)+","+ intentData[1]);
+                Log.v("client intet", passedData);
+                toCategories.putExtra("ClientId", passedData);// pass on the data
+
+                startActivity(toCategories);
+
+            }
+        });
 
 
 
