f3bbde0efe2d9b3024302e4890fbc26e8983f677
CORRU <corneliaefros@gmail.com>
2016-11-06 17:14:04 +0200

    added more tests to test activity launch

--- app/src/test/java/rainvagel/healthreporter/IntentTests.java
+++ app/src/test/java/rainvagel/healthreporter/IntentTests.java
@@ -2,8 +2,12 @@ package rainvagel.healthreporter;
 
 import android.content.Intent;
 import android.os.Build;
-import android.widget.Button;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,12 +16,16 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.fakes.RoboMenu;
 import org.robolectric.shadows.ShadowActivity;
 
+import rainvagel.healthreporter.CategoryClasses.CategoriesActivity;
 import rainvagel.healthreporter.ClientClasses.ClientActivity;
 import rainvagel.healthreporter.ClientClasses.InsertClientActivity;
 import rainvagel.healthreporter.ClientClasses.NewClientActivity;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -32,6 +40,7 @@ public class IntentTests {
     NewClientActivity newClientActivity;
     InsertClientActivity insertClientActivity;
     CategoriesActivity categoriesActivity;
+    InsertGroupActivity insertGroupActivity;
 
     @Before
     public void setup() {
@@ -39,11 +48,12 @@ public class IntentTests {
         newClientActivity = Robolectric.setupActivity(NewClientActivity.class);
         insertClientActivity = Robolectric.setupActivity(InsertClientActivity.class);
         //categoriesActivity = Robolectric.setupActivity(CategoriesActivity.class);
+        insertGroupActivity = Robolectric.setupActivity(InsertGroupActivity.class);
     }
 
     // in ClientActivity
     @Test
-    public void assertButtonClick_launchesNewClientActivity() {
+    public void assert_buttonClick_launchesNewClientActivity() {
         clientActivity.findViewById(R.id.fab1).performClick();
         Intent expectedIntent = new Intent(clientActivity, NewClientActivity.class);
         ShadowActivity shadowActivity = Shadows.shadowOf(clientActivity);
@@ -52,7 +62,7 @@ public class IntentTests {
     }
 
     @Test
-    public void assertclickOnClient_launchesCategoriesActivity() {
+    public void assert_clickOnClient_launchesCategoriesActivity() {
         ListView list = (ListView) clientActivity.findViewById(R.id.listViewClients);
         Shadows.shadowOf(list).performItemClick(0);
         Intent expectedIntent = new Intent(clientActivity, CategoriesActivity.class);
@@ -61,10 +71,20 @@ public class IntentTests {
         assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 
+    @Test
+    public void assert_longClickOnClient_launchesMenuInflater() {
+        clientActivity.findViewById(R.id.listViewClients).performLongClick();
+        MenuInflater menuInflater = new MenuInflater(clientActivity);
+        Menu menu = new RoboMenu(clientActivity);
+        menuInflater.inflate(R.menu.context_menu, menu);
+        assertNotNull(menu);
+        assertEquals("Edit", menu.findItem(R.id.cnt_mnu_edit).getTitle());
+        assertEquals("Delete", menu.findItem(R.id.cnt_mnu_delete).getTitle());
+    }
 
     // in NewClientActivity
     @Test
-    public void assertClickOnCategory_launchesInsertClientActivity() {
+    public void assert_clickOnCategory_launchesInsertClientActivity() {
         ListView list = (ListView) newClientActivity.findViewById(R.id.listViewGroups);
         Shadows.shadowOf(list).performItemClick(0);
         Intent expectedIntent = new Intent(newClientActivity, InsertClientActivity.class);
@@ -73,10 +93,19 @@ public class IntentTests {
         assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 
-    // in CategorisActivity
+    @Test
+    public void assert_clickOnFAB_launchesInsertGroupActivity() {
+        newClientActivity.findViewById(R.id.fab1).performClick();
+        Intent expectedIntent = new Intent(newClientActivity, InsertGroupActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(newClientActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+    // in CategoriesActivity
     /*
     @Test
-    public void assertClickOnCategory_launchesTestsActivity() {
+    public void assert_clickOnCategory_launchesTestsActivity() {
         ListView list = (ListView) categoriesActivity.findViewById(R.id.listView);
         Shadows.shadowOf(list).performItemClick(0);
         Intent expectedIntent = new Intent(categoriesActivity, InsertClientActivity.class);
@@ -86,7 +115,7 @@ public class IntentTests {
     }
 
     @Test
-    public void assertClickOnButton_launchesAddTestActivity() {
+    public void assert_clickOnButton_launchesAddTestActivity() {
         categoriesActivity.findViewById(R.id.floatingActionButton5).performClick();
         Intent expectedIntent = new Intent(categoriesActivity, AddTestActivity.class);
         ShadowActivity shadowActivity = Shadows.shadowOf(categoriesActivity);
@@ -94,4 +123,15 @@ public class IntentTests {
         assertTrue(actualIntent.filterEquals(expectedIntent));
     }
     */
+
+
+    // in InsertGroupActivity
+    @Test
+    public void assert_clickOnButton_launchesClientActivity() {
+        insertGroupActivity.findViewById(R.id.button_add_client).performClick();
+        Intent expectedIntent = new Intent(insertGroupActivity, ClientActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(insertGroupActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
 }
