813318fef1f3c4a0a01c4845bdcfa2f51da256ca
CORRU <corneliaefros@gmail.com>
2016-11-07 02:37:51 +0200

    testing date and group display

--- app/src/androidTest/java/rainvagel/healthreporter/EditClientTest.java
+++ app/src/androidTest/java/rainvagel/healthreporter/EditClientTest.java
@@ -25,6 +25,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.doubleClick;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
@@ -100,4 +101,63 @@ public class EditClientTest {
         assertEquals(formattedDate, cursor2.getString(updateIdx));
     }
 
+    @Test
+    public void checkIf_correctDateIsDisplayed() {
+        String[] clientColumn = {DBContract.Clients.KEY_BIRTHDATE};
+        Cursor cursor = database.getReadableDatabase().query(DBContract.Clients.TABLE_NAME, clientColumn, null,null,null,null,null);
+        int birthdayIdx = cursor.getColumnIndex(DBContract.Clients.KEY_BIRTHDATE);
+        cursor.moveToFirst();
+        String birthday = cursor.getString(birthdayIdx);
+        database.close();
+
+        String year = birthday.substring(0,4);
+        String month = birthday.substring(5,7);
+        String day = birthday.substring(8, birthday.length());
+
+        onView(withId(R.id.textview_birth_day)).check(matches(withText(day)));
+        onView(withId(R.id.textview_birth_month)).check(matches(withText(month)));
+        onView(withId(R.id.textview_birth_year)).check(matches(withText(year)));
+    }
+
+    @Test
+    public void checkIf_correctDateIsDisplayed_afterPickingNewDate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = simpleDateFormat.format(calendar.getTime());
+
+        String year1 = formattedDate.substring(0,4);
+        String month1 = formattedDate.substring(5,7);
+        String day1 = formattedDate.substring(8, formattedDate.length());
+
+        onView(withId(R.id.birthdate_picker)).perform(click());
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.textview_birth_day)).check(matches(withText(day1)));
+        onView(withId(R.id.textview_birth_month)).check(matches(withText(month1)));
+        onView(withId(R.id.textview_birth_year)).check(matches(withText(year1)));
+    }
+
+    @Test
+    public void checkIf_correctGroupIsDisplayed() {
+        String[] clientColumn = {DBContract.Clients.KEY_GROUP_ID};
+        Cursor cursor = database.getReadableDatabase().query(DBContract.Clients.TABLE_NAME, clientColumn, null, null, null, null, null);
+        int groupIdx = cursor.getColumnIndex(DBContract.Clients.KEY_GROUP_ID);
+        cursor.moveToFirst();
+        String clientGroupId = cursor.getString(groupIdx);
+        cursor.close();
+
+        String[] groupColumns = {DBContract.Groups.KEY_ID, DBContract.Groups.KEY_NAME};
+        cursor = database.getReadableDatabase().query(DBContract.Groups.TABLE_NAME, groupColumns, null, null, null, null, null);
+        groupIdx = cursor.getColumnIndex(DBContract.Groups.KEY_ID);
+        int groupNameIndex = cursor.getColumnIndex(DBContract.Groups.KEY_NAME);
+
+        String groupName = "";
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            if (clientGroupId.equals(cursor.getString(groupIdx))) {
+                groupName = cursor.getString(groupNameIndex);
+            }
+        }
+        cursor.close();
+
+        onView(withId(R.id.textview_group_name)).check(matches(withText(groupName)));
+    }
 }
