779e5eace587b643f0dd023ee8f6e47ef29e7e0a
CORRU <corneliaefros@gmail.com>
2016-12-02 14:28:32 +0200

    testing group adding and deleting

--- app/src/androidTest/java/rainvagel/healthreporter/InsertGroupToDBTest.java
+++ app/src/androidTest/java/rainvagel/healthreporter/InsertGroupToDBTest.java
@@ -13,7 +13,6 @@ import rainvagel.healthreporter.DBClasses.DBHelper;
 import rainvagel.healthreporter.DBClasses.DBQueries;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by Cornelia on 29/11/2016.
@@ -23,6 +22,8 @@ public class InsertGroupToDBTest {
     private Instrumentation instrumentation;
     private DBHelper database;
     private DBQueries queries;
+
+    private String uuid;
 
     @Before
     public void setUp() {
@@ -34,13 +35,15 @@ public class InsertGroupToDBTest {
     @Test
     public void addingGroup() {
         queries.insertGroupToDB(instrumentation.getTargetContext(), "Testgrupp1", "2017-01-01");
-        String[] groupColumns = {DBContract.Groups.KEY_ID, DBContract.Groups.KEY_NAME};
+        String[] groupColumns = {DBContract.Groups.KEY_ID, DBContract.Groups.KEY_NAME, DBContract.Groups.KEY_UPDATED, DBContract.Groups.KEY_UPLOADED};
         Cursor cursor = database.getReadableDatabase().query(DBContract.Groups.TABLE_NAME, groupColumns, null,null,null,null,null);
+        int idIdx = cursor.getColumnIndex(DBContract.Groups.KEY_ID);
         int nameIdx = cursor.getColumnIndex(DBContract.Groups.KEY_NAME);
         int dateIdx = cursor.getColumnIndex(DBContract.Groups.KEY_UPDATED);
         int uploadIdx = cursor.getColumnIndex(DBContract.Groups.KEY_UPLOADED);
         for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
             if (cursor.getString(nameIdx).equals("Testgrupp1")) {
+                uuid = cursor.getString(idIdx);
                 assertEquals("2017-01-01", cursor.getString(dateIdx));
                 assertEquals("0000-00-00", cursor.getString(uploadIdx));
             }
@@ -50,6 +53,7 @@ public class InsertGroupToDBTest {
 
     @After
     public void tearDown() {
+        queries.deleteEntryFromDB(instrumentation.getTargetContext(), DBContract.Groups.TABLE_NAME, DBContract.Groups.KEY_ID, uuid);
         database.close();
     }
 }
