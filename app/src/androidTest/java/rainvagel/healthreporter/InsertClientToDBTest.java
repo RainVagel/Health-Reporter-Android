8ec1bf25d1216d0f9d6f84115f7fb62a9488978d
CORRU <corneliaefros@gmail.com>
2016-12-02 15:31:00 +0200

    modifyed test classes

--- app/src/androidTest/java/rainvagel/healthreporter/InsertClientToDBTest.java
+++ app/src/androidTest/java/rainvagel/healthreporter/InsertClientToDBTest.java
@@ -23,6 +23,20 @@ public class InsertClientToDBTest {
     private DBHelper database;
     private DBQueries queries;
 
+    private static final String GROUP     = "University of Tartu";
+    private static final String FIRSTNAME = "Test";
+    private static final String LASTNAME  = "Client";
+    private static final String EMAIL     = "testclient@gmail.com";
+    private static final int    GENDER    = 0;    //0 - female, 1 - male
+    private static final String YEAR      = "1999";
+    private static final String MONTH     = "1";
+    private static final String DAY       = "1";
+    private static final String UPDATED   = "2016-12-31";
+    private static final String UPLOADED  = "0000-00-00";
+    private static final String BIRTHDAY  = YEAR + "-" + MONTH + "-" + DAY;
+
+    private String uuid;
+
     @Before
     public void setUp() {
         instrumentation = InstrumentationRegistry.getInstrumentation();
@@ -30,32 +44,45 @@ public class InsertClientToDBTest {
         queries = new DBQueries();
     }
 
-    // gender 0 - female, 1 - male
     @Test
     public void insertingClient() {
-        queries.insertClientToDB(instrumentation.getTargetContext(), "University of Tartu", "Mary",
-                "Jane", "maryjane@gmail.com", 0, "1986", "8", "16", "2016-08-02");
+        queries.insertClientToDB(instrumentation.getTargetContext(), GROUP, FIRSTNAME,
+                LASTNAME, EMAIL, GENDER, YEAR, MONTH, DAY, UPDATED);
 
-        String[] clientColumns = {DBContract.Clients.KEY_GROUP_ID, DBContract.Clients.KEY_FIRSTNAME,
-                DBContract.Clients.KEY_LASTNAME, DBContract.Clients.KEY_EMAIL, DBContract.Clients.KEY_BIRTHDATE,
-        DBContract.Clients.KEY_UPDATED};
+        String[] clientColumns = {DBContract.Clients.KEY_ID, DBContract.Clients.KEY_GROUP_ID, DBContract.Clients.KEY_FIRSTNAME,
+                DBContract.Clients.KEY_LASTNAME, DBContract.Clients.KEY_EMAIL, DBContract.Clients.KEY_GENDER,
+                DBContract.Clients.KEY_BIRTHDATE, DBContract.Clients.KEY_UPDATED, DBContract.Clients.KEY_UPLOADED};
 
-        Cursor cursor = database.getReadableDatabase().query(DBContract.Groups.TABLE_NAME, clientColumns, null,null,null,null,null);
+        Cursor cursor = database.getReadableDatabase().query(DBContract.Clients.TABLE_NAME, clientColumns, null,null,null,null,null);
+        int idIdx = cursor.getColumnIndex(DBContract.Clients.KEY_ID);
         int groupIdx = cursor.getColumnIndex(DBContract.Clients.KEY_GROUP_ID);
         int firstNameIdx = cursor.getColumnIndex(DBContract.Clients.KEY_FIRSTNAME);
         int lastNameIdx = cursor.getColumnIndex(DBContract.Clients.KEY_LASTNAME);
         int emailIdx = cursor.getColumnIndex(DBContract.Clients.KEY_EMAIL);
+        int genderIdx = cursor.getColumnIndex(DBContract.Clients.KEY_GENDER);
         int birthdayIdx = cursor.getColumnIndex(DBContract.Clients.KEY_BIRTHDATE);
         int updateIdx = cursor.getColumnIndex(DBContract.Clients.KEY_UPDATED);
+        int uploadIdx = cursor.getColumnIndex(DBContract.Clients.KEY_UPLOADED);
 
         for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            //TODO
+            if (cursor.getString(groupIdx).equals(GROUP)) {
+                if (cursor.getString(firstNameIdx).equals(FIRSTNAME)) {
+                    uuid = cursor.getString(idIdx);
+                    assertEquals(LASTNAME, cursor.getString(lastNameIdx));
+                    assertEquals(EMAIL, cursor.getString(emailIdx));
+                    assertEquals(GENDER, cursor.getInt(genderIdx));
+                    assertEquals(BIRTHDAY, cursor.getString(birthdayIdx));
+                    assertEquals(UPDATED, cursor.getString(updateIdx));
+                    assertEquals(UPLOADED, cursor.getString(uploadIdx));
+                }
+            }
         }
         cursor.close();
     }
 
     @After
     public void tearDown() {
+        queries.deleteEntryFromDB(instrumentation.getTargetContext(), DBContract.Clients.TABLE_NAME, DBContract.Clients.KEY_ID,uuid);
         database.close();
     }
 }
