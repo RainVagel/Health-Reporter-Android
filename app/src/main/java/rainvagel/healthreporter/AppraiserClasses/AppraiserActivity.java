190a168b0fcc874cb4f86c61dba443409902cbde
rainvagel <rain.vagel@gmail.com>
2016-12-04 17:24:42 +0200

    Created NewAppraiserActivity. Now you can create appraisers in the app. closes #208

--- app/src/main/java/rainvagel/healthreporter/AppraiserClasses/AppraiserActivity.java
+++ app/src/main/java/rainvagel/healthreporter/AppraiserClasses/AppraiserActivity.java
@@ -1,7 +1,8 @@
-package rainvagel.healthreporter;
+package rainvagel.healthreporter.AppraiserClasses;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,12 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import rainvagel.healthreporter.ClientClasses.ClientActivity;
 import rainvagel.healthreporter.DBClasses.DBAppraiserTransporter;
 import rainvagel.healthreporter.DBClasses.DBQueries;
+import rainvagel.healthreporter.R;
 
 public class AppraiserActivity extends AppCompatActivity {
 
@@ -27,6 +28,7 @@ public class AppraiserActivity extends AppCompatActivity {
     Map<String, String> appraiserIdToNames;
     Map<String, String> appraiserNameToId = new HashMap<>();
     ListView listView;
+    private FloatingActionButton floatingActionButton;
 
     ArrayAdapter adapter;
 
@@ -34,6 +36,15 @@ public class AppraiserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appraiser);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab_appraiser);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AppraiserActivity.this, NewAppraiserActivity.class);
+                startActivityForResult(intent, 2);
+            }
+        });
 
         DBQueries dbQueries = new DBQueries();
         DBAppraiserTransporter dbAppraiserTransporter = dbQueries.getAppraisersFromDB(this);
@@ -61,5 +72,18 @@ public class AppraiserActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                Intent toClientActivity = new Intent(AppraiserActivity.this, ClientActivity.class);
+                String passedData = data.getStringExtra("uuid");
+                toClientActivity.putExtra("appraiserID", passedData);
+                setResult(Activity.RESULT_OK, toClientActivity);
+                finish();
+            }
+        }
     }
 }
